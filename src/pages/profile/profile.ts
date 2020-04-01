58c8a954620591a04fb1072e164423231cf97ebc
Anderson Misson <missondeveloper@gmail.com>
2020-04-01 20:25:32 -0300

    Fazendo Upload da Foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -29,6 +29,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() { // Já carregar junto com a página
+    this.loadData();
+  }
+
+  loadData() {
     let localUser = this.storage.getLocalUser();
     if(localUser && localUser.email){
       this.clienteService.findByEmail(localUser.email)
@@ -71,6 +75,20 @@ export class ProfilePage {
      this.cameraOn = false;
     }, (err) => {
     });
+  }
+
+  sendPicture() {
+    this.clienteService.uploadPicture(this.picture)
+      .subscribe(response => {
+        this.picture = null;
+        this.loadData();
+      },
+      error => {
+      });
+  }
+
+  cancel() {
+    this.picture = null;
   }
 
 }
